8a360c6195224430403daf0adc73bd02bf849f3a
luizaaragao076-design <luizaaragao076@gmail.com>
2026-01-31 15:48:44 -0300

    Add files via upload

---
+++ aula_4.py
@@ -0,0 +1,49 @@
+### 1 - Crie um programa para efetuar a leitura de um número inteiro e apresentar o resultado do quadrado deste número.
+print("Resultado ao quadrado")
+print("......"*10)
+n1 = float(input("Informe o número, por gentileza: "))
+num_quad = n1**2
+print(num_quad," é o valor quando ", n1, "é elevado ao quadrado.")
+print()
+print()
+print()
+
+### 2 - Crie duas variáveis para armazenar seu primeiro nome e sobrenome. Em seguida, concatene-as para formar seu nome completo e exiba o resultado.
+print("NOME COMPLETO")
+print("......"*10)
+
+nome = input("Qual o seu nome? ")
+sobrenome = input("Qual o seu sobrenome? ")
+ult_nome = input("Por fim, qual o seu último nome? ")
+print()
+print("Olá, boa tarde ", nome, sobrenome,ult_nome,"!")
+print()
+print()
+print()
+### 3 - Peça ao usuário para digitar dois números inteiros e armazene-os em variáveis. Realize a concatenação desses números como strings e exiba o resultado.
+print("NÚMEROS")
+print("......"*10)
+num1= input("Informe um número inteiro: ")
+num2= input("Informe um número inteiro: ")
+
+num3 = str(print(num1+num2))
+print()
+print()
+print()
+### 4 - Crie uma variável para armazenar a palavra "Python". Em seguida, adicione um número inteiro ao final da palavra usando a concatenação e exiba o resultado.
+print("Python concatenado")
+print("......"*10)
+py= ("Python")
+num4= input("Informe um número inteiro: ")
+str(print(py + num4))
+print()
+print()
+print()
+### 5 - Declare uma variável contendo uma frase. Em seguida, peça ao usuário para digitar uma palavra e concatene essa palavra no final da frase. Exiba o resultado.
+print("Frase concatenada")
+print("......"*10)
+ajuda = input("Olá, boa tarde. Como posso te ajudar? ")
+resposta = print("Irei te encaminha para que seja resolvido sua " + ajuda)
+print()
+print()
+print()
