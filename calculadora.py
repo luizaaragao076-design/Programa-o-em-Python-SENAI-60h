56180275c56ced7d7cc089c70eade03521a1d95a
luizaaragao076-design <luizaaragao076@gmail.com>
2026-01-31 14:33:02 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,26 @@
+print("Atividade: calculadora")
+print("....."*10)
+print()
+nome = input("Por gentileza, qual o seu nome? ")
+print()
+print("Boa tarde " + nome +"!")
+print()
+n1= float(input("por favor, informe o número desejado: "))
+n2 = float(input("Informe o segundo número desejado: "))
+
+print()
+
+print()
+soma = float(n1+n2)
+
+print("A soma desses valores é igual á ", soma)
+
+sub = float(n1-n2)
+print("A subtração dos valores é igual á ", sub)
+
+
+mult = float(n1*n2)
+print("A multiplicação desses valores é igual á ", mult)
+
+div = float(n1/n2)
+print("A divisão dos valores é igual á ", div)
