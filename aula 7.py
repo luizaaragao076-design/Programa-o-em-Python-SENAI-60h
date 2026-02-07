5ed1f04c16688e975fde964192f9699253263467
luizaaragao076-design <luizaaragao076@gmail.com>
2026-02-07 10:48:07 -0300

    Add files via upload

---
+++ aula 7.py
@@ -0,0 +1,30 @@
+# Exercício 
+print("Exercício 0:")
+for numero in range(2, 21, 2):
+    print(numero)
+
+# Exercício 1
+print("\nExercício 1:")
+numeros = list(range(1, 11))
+print(numeros)
+
+# Exercício 2
+print("\nExercício 2:")
+print(numeros[2])  # terceiro elemento (índice 2)
+
+# Exercício 3
+print("\nExercício 3:")
+numeros.append(9)
+print(numeros)
+
+# Exercício 4
+print("\nExercício 4:")
+numeros.remove(5)
+print(numeros)
+
+# Exercício 5
+print("\nExercício 5:")
+carros = ["Fusca", "Civic", "Corolla"]
+lista_concatenada = numeros + carros
+print(lista_concatenada)
+
